262da8006e02761ca5902bf8775de3a07ad5aae9
MNFajarAlfian2107 <alfianfajar082@gmail.com>
2026-06-05 23:24:50 +0700

    Delete TopUPK2 directory

--- TopUPK2/topup/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from .models import Transaction
-
-@admin.register(Transaction)
-class TransactionAdmin(admin.ModelAdmin):
-    list_display = ('game', 'nominal', 'payment', 'total', 'created_at')
-    list_filter = ('payment', 'created_at')
